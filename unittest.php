766e6bed586936215413968bdcb9b8aedd963a37
TeachSquad <66652262+TeachSquad@users.noreply.github.com>
2020-06-15 02:38:26 +0530

    Add files via upload

--- unittest.php
+++ unittest.php
@@ -1,66 +1,190 @@
-    <html>  
-      <head>  
-           <title>Dynamically Add or Remove input fields in PHP with JQuery</title>  
-           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-      </head>  
-      <body>  
-           <div class="container">  
-                <br />  
-                <br />  
-                <h2 align="center">Dynamically Add or Remove input fields in PHP with JQuery</h2>  
-                <div class="form-group">  
-                     <form name="add_name" id="add_name">  
-                          <div class="table-responsive">  
-                               <table class="table table-bordered" id="dynamic_field">  
-                                <tr>  
-                                <td><select class="form-control" id="ngo_name" name="ngo_name[]">
-				        	    <option selected class="bg-light p-5 contact-form"><p>Which NGO you want to donate</p></option>
-				        	    <option value="abc">abc</option>
-					            <option value="def">def</option>
-					            <option value="ghi">ghi</option>
-					            </select>
-                                </td>
-                                         <td><input type="text" name="quantity[]" placeholder="quantity" class="form-control name_list" /></td>  
-                                         <td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>  
-                                    </tr>  
-                               </table>  
-                               <input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />  
-                          </div>  
-                     </form>  
-                </div>  
-           </div>  
-      </body>  
- </html>  
- <script>  
- $(document).ready(function(){  
-      var i=1;  
-      $('#add').click(function(){  
-           i++;  
-           $('#dynamic_field').append('<tr id="row'+i+'"><td><select class="form-control" id="ngo_name" name="ngo_name[]">'+
-				        	    '<option selected class="bg-light p-5 contact-form"><p>Which NGO you want to donate</p></option>'+
-				        	    '<option value="abc">abc</option>'+
-					            '<option value="def">def</option>'+
-					            '<option value="ghi">ghi</option>'+
-					            '</select></td><td><input type="text" name="quantity[]" placeholder="Quantity" class="form-control name_list" /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
-      });  
-      $(document).on('click', '.btn_remove', function(){  
-           var button_id = $(this).attr("id");   
-           $('#row'+button_id+'').remove();  
-      });  
-      $('#submit').click(function(){            
-           $.ajax({  
-                url:"name.php",  
-                method:"POST",  
-                data:$('#add_name').serialize(),  
-                success:function(data)  
-                {  
-                     alert(data);  
-                     $('#add_name')[0].reset();  
-                }  
-           });  
-      });  
- });  
- </script>
-   
+<?php
+
+
+function wh_log($msg){
+    $logfile='log/log_'.date("Y-m-d").'.log';
+    file_put_contents($logfile,$msg. "\n",FILE_APPEND);
+
+
+}
+?>
+<?php
+function databaseTest(){
+    include('connection.php');
+
+    if($conn){
+        wh_log("#############################################");
+        wh_log("The testing of database connection is succeed");
+        wh_log("It has passed the test case");
+        wh_log("Test case server name:".$servername);
+        wh_log("Test case user name:".$username);
+        wh_log("Test case database name:".$dbname);
+        wh_log("#############################################");
+    }
+    else{
+        wh_log("#############################################");
+        wh_log("The testing of database connection is failed");
+        wh_log("It has not passed the test case");
+        wh_log("Test case server name:".$servername);
+        wh_log("Test case user name:".$username);
+        wh_log("Test case database name:".$dbname);
+        wh_log("#############################################");
+    }
+}
+databaseTest();
+
+
+
+function admin_verify(){
+    include('admin_verification.php');
+    $passed=0;
+    $failed=0;
+    $count=0;
+    $users=array("shubham","Shubham122","");
+    $passwords= array("kukki303","ku");
+    foreach($users as $user){
+        foreach($passwords as $password){
+            $count++;
+            if(admin_verification($user,$password)){
+                wh_log("#############################################");
+                wh_log("The testing of admin verification is succeed");
+                wh_log("It has passed the test case");
+                wh_log("Test case user name:".$user);
+                wh_log("Test case password name:".$password);
+                wh_log("#############################################");
+                $passed++;
+            }
+            else{
+                wh_log("#############################################");
+                wh_log("The testing of admin verification is failed");
+                wh_log("It has not passed the test case");
+                wh_log("Test case user name:".$user);
+                wh_log("Test case password name:".$password);
+                wh_log("#############################################");
+                $failed++;
+            }
+
+
+        }
+    }
+    wh_log("Total test case executed ".$count);
+    wh_log("Total test case passed ".$passed);
+    wh_log("Total test case failed ".$failed);    
+    
+}
+admin_verify();
+
+function ngo_verify(){
+    include('ngo_verification.php');
+    $passed=0;
+    $failed=0;
+    $count=0;
+
+    $users=array("shubham","abdshubham13@gmail.com","");
+    $passwords= array("kukki303","1234");
+
+    foreach($users as $user){
+        foreach($passwords as $password){
+            $count++;
+            if(ngo_verification($user,$password)){
+                wh_log("#############################################");
+                wh_log("The testing of ngo verification is succeed");
+                wh_log("It has passed the test case");
+                wh_log("Test case user name:".$user);
+                wh_log("Test case password name:".$password);
+                wh_log("#############################################");
+                $passed++;
+            }
+            else{
+                wh_log("#############################################");
+                wh_log("The testing of ngo verification is failed");
+                wh_log("It has not passed the test case");
+                wh_log("Test case user name:".$user);
+                wh_log("Test case password name:".$password);
+                wh_log("#############################################");
+                $failed++;
+            }
+
+
+        }
+
+    }
+    wh_log("Total test case executed ".$count);
+    wh_log("Total test case passed ".$passed);
+    wh_log("Total test case failed ".$failed);
+    
+}
+ngo_verify();
+
+
+function mail_test(){
+   // $doner_email="abdshubham13@gmail.com";
+    //$doner_name="shubham";
+    include('mail.php');
+    $passed=0;
+    $failed=0;
+    $count=0;
+
+    $doner_emails=array("shubham","abdshubham13@gmail.com","");
+    //$doner_names= array("kukki303","1234");
+
+    foreach($doner_emails as $doner_email){
+        $count++;
+            if(!$mail->send()) {
+                wh_log("#############################################");
+                wh_log("The testing of email transfer is failed");
+                wh_log("It has not passed the test case");
+                wh_log("Test case user email:".$doner_email);
+                wh_log("Test case user name:".$doner_name);
+                wh_log("#############################################");
+                $failed++;
+            } else {
+                wh_log("#############################################");
+                wh_log("The testing of email transfer is succeed");
+                wh_log("It has passed the test case");
+                wh_log("Test case user name:".$doner_email);
+                wh_log("Test case user name:".$doner_name);
+                wh_log("#############################################");
+                $passed++
+            }
+        
+
+        }
+
+        wh_log("Total test case executed ".$count);
+        wh_log("Total test case passed ".$passed);
+        wh_log("Total test case failed ".$failed);
+    
+
+ }
+mail_test();
+
+
+
+
+
+function ngo_registration(){
+    include('ngo_registration.php');
+    if(ngo_registration($name, $chairman, $contact_no, $address, $email, $pass, $establishment_year)){
+                wh_log("#############################################");
+                wh_log("The testing of email transfer is failed");
+                wh_log("It has not passed the test case");
+                wh_log("Test case user email:".$email);
+                wh_log("Test case user name:".$name);
+                wh_log("#############################################");
+                $passed++;
+    }
+    else{
+        wh_log("#############################################");
+        wh_log("The testing of email transfer is succeed");
+        wh_log("It has passed the test case");
+        wh_log("Test case user name:".$name);
+        wh_log("Test case user name:".$email);
+        wh_log("#############################################");
+        $failed++
+
+    }
+}
+ngo_registration();
+?> 
+
